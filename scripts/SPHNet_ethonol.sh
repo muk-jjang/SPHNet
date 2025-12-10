e40edda473f4fb2148f02c64814e835c07f8a63a
muk-jjang <whgdk0911@khu.ac.kr>
2025-12-10 02:50:48 +0000

    add: train, test scripts

---
+++ scripts/SPHNet_ethonol.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd -- "$(dirname "$0")" && pwd)"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+
+cd "$ROOT_DIR" || exit 1
+export HYDRA_FULL_ERROR=1
+export CUDA_VISIBLE_DEVICES=0
+python pipelines/train.py --config-name=ethanol.yaml 
+    
