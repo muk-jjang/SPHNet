1aaef361bc890eb8284ef8eb265edb3cf095b9cc
muk-jjang <whgdk0911@khu.ac.kr>
2025-12-17 16:44:43 +0900

    add: test script

---
+++ scripts/aspirin_dump.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd -- "$(dirname "$0")" && pwd)"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+
+cd "$ROOT_DIR" || exit 1
+export HYDRA_FULL_ERROR=1
+export CUDA_VISIBLE_DEVICES=1
+python pipelines/test.py \
+--config-name=aspirin.yaml \
+save_output_dump=true \
+inference_batch_size=216 \
+ckpt_path=/nas/seongjun/sphnet/ \
+log_dir=/nas/seongjun/sphnet/
