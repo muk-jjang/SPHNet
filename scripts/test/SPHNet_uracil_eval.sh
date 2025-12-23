7084d1ea5dd0053e9edd14a83e568240c6037e03
muk-jjang <whgdk0911@khu.ac.kr>
2025-12-23 14:46:56 +0900

    fix: scripts

--- scripts/test/SPHNet_uracil_eval.sh
+++ scripts/test/SPHNet_uracil_eval.sh
@@ -3,13 +3,14 @@
 # Always run from repo root regardless of call location
 ROOT_DIR="/home/sungjun/repos/SPHNet"
 cd "$ROOT_DIR" || exit 1
-export HYDRA_FULL_ERROR=1
-export OMP_NUM_THREADS=32
-export MKL_NUM_THREADS=32
-export OPENBLAS_NUM_THREADS=32
-export NUMEXPR_NUM_THREADS=32
 
-python md17_evaluation_customv2.py \
+PYTHON_PATH="/home/chanhui-lee/.conda/envs/sphnet-gpu4pyscf/bin/python"
+
+export HYDRA_FULL_ERROR=1
+
+"$PYTHON_PATH" md17_evaluation_customv2.py \
 --dir_path=/nas/seongjun/sphnet/uracil/output_dump \
---num_procs=5 \
+--num_procs=1 \
+--size_limit=-1 \
+--use_gpu=6
     
--- scripts/test/salicylic_acid_test.sh
+++ scripts/test/salicylic_acid_test.sh
@@ -3,15 +3,14 @@
 # Always run from repo root regardless of call location
 ROOT_DIR="/home/sungjun/repos/SPHNet"
 cd "$ROOT_DIR" || exit 1
-export HYDRA_FULL_ERROR=1
-export OMP_NUM_THREADS=32
-export MKL_NUM_THREADS=32
-export OPENBLAS_NUM_THREADS=32
-export NUMEXPR_NUM_THREADS=32
 
-python md17_evaluation_customv2.py \
---dir_path=/nas/seongjun/sphnet/salicylic_acid/output_dump_batch \
+PYTHON_PATH="/home/chanhui-lee/.conda/envs/sphnet-gpu4pyscf/bin/python"
+
+export HYDRA_FULL_ERROR=1
+
+"$PYTHON_PATH" md17_evaluation_customv2.py \
+--dir_path=/nas/seongjun/sphnet/salicylic_acid/output_dump \
 --num_procs=1 \
---debug \
---size_limit=1 \
+--size_limit=-1 \
+--use_gpu=3
     
