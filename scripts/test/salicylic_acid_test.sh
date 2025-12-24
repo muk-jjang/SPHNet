cc4ca846ac933874e9c587679cd34b48928460b5
muk-jjang <whgdk0911@khu.ac.kr>
2025-12-24 18:00:13 +0900

    update

--- scripts/test/salicylic_acid_test.sh
+++ scripts/test/salicylic_acid_test.sh
@@ -4,17 +4,15 @@
 ROOT_DIR="/home/sungjun/repos/SPHNet"
 cd "$ROOT_DIR" || exit 1
 
-PYTHON_PATH="/home/chanhui-lee/.conda/envs/sphnet-gpu4pyscf/bin/python"
-
 export HYDRA_FULL_ERROR=1
 export OMP_NUM_THREADS=32
 export MKL_NUM_THREADS=32
 export OPENBLAS_NUM_THREADS=32
 export NUMEXPR_NUM_THREADS=32
 
-"$PYTHON_PATH" md17_evaluation_customv2.py \
+python md17_evaluation_customv2.py \
 --dir_path=/nas/seongjun/sphnet/salicylic_acid/output_dump \
---num_procs=5 \
+--num_procs=60 \
 --size_limit=-1 \
 --use_gpu=-1 \
 --do_new_calc
