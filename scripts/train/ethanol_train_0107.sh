f0208959f1a1568094ab41008bb0c4b0259ebf90
chless <chlessling@gmail.com>
2026-01-07 18:33:10 +0900

    add exp bash

---
+++ scripts/train/ethanol_train_0107.sh
@@ -0,0 +1,12 @@
+devices=$1
+dataset_name=ethanol
+python pipelines/train.py \
+--config-name=${dataset_name}.yaml \
+save_output_dump=true \
+job_id=${dataset_name}-36M-learn-pyscf-h \
+inference_batch_size=128 \
+ckpt_path=/nas/seongjun/sphnet \
+log_dir=/nas/seongjun/sphnet \
+devices=${devices} \
+model=sphnet-36M \
+wandb.wandb_api_key=${WANDB_API_KEY_KU_AI4SIM}
